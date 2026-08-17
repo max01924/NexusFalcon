1bddfcdae2bb694f5d1041b09cb3387775d70d2f
Max Meusel <max.meusel01@icloud.com>
2026-08-17 17:40:14 +0200

    gui skelett

--- src/gui.py
+++ src/gui.py
@@ -25,4 +25,69 @@ class EmailGui():
         y = (screen_height - window_height) // 2
         self.root.geometry(f"{window_width}x{window_height}+{x}+{y}")
 
-    def _frames (self)
+    def _window(self):
+        self.paned = ttk.PanedWindow(self.root, orient="horizontal")
+        self.paned.pack(fill="both", expand=True)
+    
+        self.frame_email_list = ttk.Frame(self.paned)
+        self.frame_email_details = ttk.Frame(self.paned)
+        self.frame_email_list.pack(side="left", fill="both", expand="True")
+        self.frame_email_details.pack(side="right", fill="both", expand="True")
+
+        self.paned.add(self.frame_email_list, weight=1)
+        self.paned.add(self.frame_email_details, weight=2)
+
+    def _email_list(self):
+        treeview = ttk.Treeview(self.frame_email_list, columns="sender, subject, date_received", show="headings", selectmode="browse")
+        treeview.heading("sender", text="Sender")
+        treeview.heading("subject", text="Subject")
+        treeview.heading("date_received", text="Date Received")
+
+        treeview.column("sender", anchor="w")
+        treeview.column("subject", anchor="w")
+        treeview.column("date_received", anchor="w")
+
+    def _email_details(self):
+        self.label_sender = ttk.Label(
+            self.frame_email_details,
+            text=self.sender,
+            wraplength=self.WINDOW_WIDTH - 20,
+            font=("Helvetica", 10, "italic")
+        )
+        self.label_subject = ttk.Label(
+            self.frame_email_details,
+            text=self.subject,
+            wraplength=self.WINDOW_WIDTH - 20,
+            font=("Helvetica", 14, "normal")
+        )
+        self.date_received = ttk.Label(
+            self.frame_email_details,
+            text=self.date_received
+            wraplength=self.WINDOW_WIDTH - 20,
+            font=("Helvetica", 10, "normal")
+        )
+        self.label_full_body = ttk.Label(
+            self.frame_email_details,
+            text=self.subject,
+            wraplength=self.WINDOW_WIDTH - 20,
+            font=("Helvetica", 15, "normal")
+        )
+        self.label_ai_tag_sentence = ttk.Label(
+            self.frame_email_details,
+            text=self.ai_tag_sentence,
+            wraplength=self.WINDOW_WIDTH - 20,
+            font=("Helvetica", 15, "normal")
+        )
+        self.label_body_summary = ttk.Label(
+            self.frame_email_details,
+            text=self.body_summary,
+            wraplength=self.WINDOW_WIDTH - 20,
+            font=("Helvetica", 15, "normal")
+        )
+        
+        self.label_sender.pack(fill="x", padx=10, pady=2)
+        self.label_subject.pack(fill="x", padx=10, pady=2)
+        self.label_date_received.pack(fill="x", padx=10, pady=2)
+        self.label_full_body.pack(fill="x", padx=10, pady=2)
+        self.label_ai_tag_sentence.pack(fill="x", padx=10, pady=2)
+        self.label_body_summary.pack(fill="x", padx=10, pady=2)
